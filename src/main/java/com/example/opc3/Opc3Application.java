9ec29ce88e56a3a921bbf5352860dfaf97bc7226
kose25 <carlosjosepg@ufps.edu.co>
2020-06-18 06:39:30 -0500

    primer commit

---
+++ src/main/java/com/example/opc3/Opc3Application.java
@@ -0,0 +1,13 @@
+package com.example.opc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Opc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opc3/Opc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Opc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
